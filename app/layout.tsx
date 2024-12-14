a6908faa1c675c7928ec72d79373f0cc90457551
moein samani <moeinsamani1992@gmail.com>
2024-12-14 21:33:13 +0100

    tree shacking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ClerkProvider } from '@clerk/nextjs';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
