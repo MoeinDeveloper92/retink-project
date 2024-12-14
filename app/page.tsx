55f227f66c1c7195d701ed949774a15e0e06959f
moein samani <moeinsamani1992@gmail.com>
2024-12-14 18:00:03 +0100

    last change

--- app/page.tsx
+++ app/page.tsx
@@ -13,14 +13,14 @@ const page = async () => {
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="max-w-7xl mx-auto flex flex-col items-center gap-8">
-        <h1 className="text-6xl text-white">
+        <h1 className="text-6xl text-white max-sm:text-2xl max-sm:text-center">
           Welcome To Image Generator Web app
         </h1>
-        <h2 className="text-3xl text-white">
-          To use This web app you must need to first Authenticated.
+        <h2 className="text-3xl text-white max-sm:text-xl max-sm:text-center">
+          To use This web app you must need to be Authenticated.
         </h2>
-        <h3 className="text-2xl text-white">
-          Authenticatio is simply mage by Google OAuth
+        <h3 className="text-2xl text-white max-sm:text-xl max-sm:text-center">
+          Authentication is simply mage by Google OAuth
         </h3>
         <Button size={'lg'} asChild>
           <Link href={'/sign-in'}>Let's Get Started</Link>
