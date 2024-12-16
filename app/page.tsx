cf2c141e46c9fdb40e2a86aff17bc4d797222cd0
moein samani <moeinsamani1992@gmail.com>
2024-12-16 14:14:14 +0100

    API Key has limited api call range

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const page = async () => {
     <div className="h-screen flex flex-col  justify-center items-center">
       <div className="max-w-7xl mx-auto flex flex-col items-center gap-8 flex-1 mt-32">
         <h1 className="text-6xl text-white max-sm:text-2xl max-sm:text-center">
-          Welcome To Image Generator Web app
+          Welcome To Image Generator Web app.
         </h1>
         <h2 className="text-2xl text-white max-sm:text-xl max-sm:text-center">
           In the production version authentication and account management will
